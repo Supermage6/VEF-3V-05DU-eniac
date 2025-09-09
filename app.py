10e5a524a1173cdd3894b3a4b801487c31f8ace7
tomas <regsofnar@gmail.com>
2025-09-09 11:05:15 +0000

    database og template

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+klubbar = [
+    {'nafn': 'Fighting Game Klúbbur', 'stofa': '203', 'formadur': 'Matt', 'desc': 'Klúbbur fyrir alla sem hafa áhuga á bardaga tölvuleikjum.'},
+    {'nafn': 'Furry Klúbbur', 'stofa': '206', 'formadur': 'Aron', 'desc': 'Klúbbur fyrir alla sem hafa áhuga á furry menningu.'},
+]
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/klubbar')
+def klubbar():
+    return render_template('klubbar.html')
+
 
