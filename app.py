4fc96a73954b6f1a40baf0290080887e0fcaf8b9
tomas <regsofnar@gmail.com>
2025-09-09 11:26:48 +0000

    database og templates

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 klubbar = [
     {'nafn': 'Fighting Game Klúbbur', 'stofa': '203', 'formadur': 'Matt', 'desc': 'Klúbbur fyrir alla sem hafa áhuga á bardaga tölvuleikjum.'},
     {'nafn': 'Furry Klúbbur', 'stofa': '206', 'formadur': 'Aron', 'desc': 'Klúbbur fyrir alla sem hafa áhuga á furry menningu.'},
+    {'nafn': 'Tónlistarklúbbur', 'stofa': 'Hátíðarsalur', 'formadur': 'Emily', 'desc': 'Klúbbur fyrir alla sem hafa áhuga á tónlist og spilamennsku.'},
+    {'nafn': 'Nördaklúbbur', 'stofa': '202', 'formadur': 'Ösp og Kormákur', 'desc': 'Klúbbur fyrir alla nörda og áhugafólk um ýmislegt nördalegt.'},
+    {'nafn': 'Anime og Cosplay Klúbbur', 'stofa': '205', 'formadur': 'Sky', 'desc': 'Klúbbur fyrir alla sem hafa áhuga á anime og cosplay.'},
+    {'nafn': 'Warhammer Klúbbur', 'stofa': '302', 'formadur': '???', 'desc': 'Klúbbur fyrir alla sem hafa áhuga á Warhammer borðspilinu.'},
+    {'nafn': 'D&D Klúbbur', 'stofa': 'fer eftir hóp', 'formadur': '???', 'desc': 'Klúbbur fyrir alla sem hafa áhuga á Dungeons & Dragons borðspilinu.'}
 ]
 
 @app.route('/')
@@ -13,6 +18,11 @@ def home():
 
 @app.route('/klubbar')
 def klubbar():
-    return render_template('klubbar.html')
+    return render_template('klubbar.html', k = klubbar)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
